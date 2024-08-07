b9f8e8ed96aec31b69e21d0dec074ad7f6dff654
markuschue <markuschue@gmail.com>
2024-08-07 15:57:47 +0100

    added possibility to use combined data or each filter

--- optimizer.py
+++ optimizer.py
@@ -1,38 +1,14 @@
 # Module used for optimizing the number of observations needed for a given star to reach a confident period estimation.
-import os
-
 import matplotlib.pyplot as plt
 import pandas as pd
-from astropy.table import Table
 
-from utils.data_utils import get_star_lookup_data
-from utils.period_utils import compute_period, prepareTable
-
-
-def get_star_photometry(star_id: str) -> list[pd.DataFrame]:
-    """
-    Get the prepared photometry data for a given star.
-    :param star_id: The ID of the star to get the photometry data for.
-    :return: The photometry data for the given star.
-    """
-    PHOTOMETRY_PATH = './data/photometry/' + star_id
-    photometry_data = {}
-    for photometry_file in os.listdir(PHOTOMETRY_PATH):
-        if photometry_file.startswith('cat_'):
-            photometry_data[photometry_file.split('.')[0].split(
-                '_')[-1]] = prepareTable(PHOTOMETRY_PATH+'/'+photometry_file, 1)
-    varid = Table.read(PHOTOMETRY_PATH+"/finalIDs_" +
-                       star_id+".fits", hdu=1)[0][0]
-    filtered_data = {}
-    pd.options.mode.chained_assignment = None
-    for key in photometry_data:
-        filtered_data[key] = photometry_data[key].loc[photometry_data[key].ID == varid, :]
-    return filtered_data
+from utils.data_utils import get_star_lookup_data, get_star_photometry
+from utils.period_utils import compute_period
 
 
 def get_periods_by_observation_number(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Calculate the periods by number of observations for a given star, 
+    Calculate the periods by number of observations for a given star,
     using a sample of the data with increasing size on each step.
     :param data: pd.DataFrame containing the data for the given star.
     :return: Dict containing the periods by observation number.
@@ -59,29 +35,39 @@ def get_gaia_period(star_id: str) -> float:
     return gaia_data['rrl.pf'].values[0]
 
 
-def plot_periods_by_observation_number(periods_by_observation_number: pd.DataFrame, title: str | None = None):
+def plot_periods_by_observation_number(periods_by_observation_number: dict, gaia_period: float, title: str = None):
     """
     Plot the periods by observation number for a given star.
-    :param periods_by_observation_number: Dict containing the periods by observation number.
+    :param periods_by_observation_number: Dict containing the periods by observation number where each key is a filter, or combination of filters.
+    :param title: The title of the plot.
     """
     plt.figure().set_figwidth(10)
-    plt.plot(periods_by_observation_number.keys(),
-             periods_by_observation_number.values())
-    plt.plot([0, max(periods_by_observation_number.keys())], [
-             get_gaia_period(star_id), get_gaia_period(star_id)], label='Gaia Period')
     plt.xlabel('Number of Observations')
     plt.ylabel('Period')
-    plt.legend(['Estimated Period', 'Gaia Period'])
     if title is not None:
         plt.title(title)
+    for key in periods_by_observation_number:
+        plt.plot(periods_by_observation_number[key].keys(),
+                 periods_by_observation_number[key].values(), label=key)
+    plt.plot([0, max(periods_by_observation_number[key].keys())], [gaia_period, gaia_period],
+             label='Gaia Period', linestyle='--')
+    plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    star_id = 'GAIA04_2051756764073824512'
-    photometry_data = get_star_photometry(star_id)
-    for key in photometry_data:
-        periods_by_observation_number = get_periods_by_observation_number(
-            photometry_data[key])
-        plot_periods_by_observation_number(
-            periods_by_observation_number, star_id + ' Periods by Observation Number for filter ' + key)
+    STAR_ID = 'GAIA03_1360607637502749440'
+    PHOTOMETRY_PATH = 'data/photometry/' + STAR_ID
+    ONLY_COMBINED = False
+
+    photometry_data = get_star_photometry(PHOTOMETRY_PATH)
+    combined_photometry = pd.concat(photometry_data.values())
+    periods_by_observation_number = {
+        "Combined": get_periods_by_observation_number(combined_photometry)
+    }
+    if not ONLY_COMBINED:
+        for key in photometry_data:
+            periods_by_observation_number[key] = get_periods_by_observation_number(
+                photometry_data[key])
+    plot_periods_by_observation_number(
+        periods_by_observation_number, get_gaia_period(STAR_ID), STAR_ID + ' Periods by Observation Number')
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,5 +1,11 @@
+import os
+from pathlib import Path
+
 import pandas as pd
+from astropy.table import Table
 from numpy import log
+
+from utils.period_utils import prepareTable
 
 
 def get_star_lookup_data(gaia_id: str) -> pd.DataFrame:
@@ -20,3 +26,24 @@ def flux_to_magnitude_error(flux: float, flux_error: float) -> float:
     :return: The magnitude error value.
     """
     return abs(-2.5 / flux / log(10)) * flux_error
+
+
+def get_star_photometry(photometry_path: str) -> dict:
+    """
+    Get the prepared photometry data for a given star as a dict of dataframes for each filter.
+    :param photometry_path: The path to the photometry data for the given star. 
+    :return: The photometry data for the given star.
+    """
+    START_ID = Path(photometry_path).stem
+    photometry_data = {}
+    for photometry_file in os.listdir(photometry_path):
+        if photometry_file.startswith('cat_'):
+            photometry_data[photometry_file.split('.')[0].split(
+                '_')[-1]] = prepareTable(photometry_path+'/'+photometry_file, 1)
+    varid = Table.read(photometry_path+"/finalIDs_" +
+                       START_ID+".fits", hdu=1)[0][0]
+    pd.options.mode.chained_assignment = None
+    filtered_data = {}
+    for key in photometry_data:
+        filtered_data[key] = photometry_data[key].loc[photometry_data[key].ID == varid, :]
+    return filtered_data

8315eb4b234331e9dd27d43c22eb8a51aae75c12
markuschue <markuschue@gmail.com>
2024-08-12 11:55:50 +0100

    added multithreading to the get_periods_by_observation_number function

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,6 @@
 # Module used for optimizing the number of observations needed for a given star to reach a confident period estimation.
+import concurrent.futures
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -6,22 +8,41 @@ from utils.data_utils import get_star_lookup_data, get_star_photometry
 from utils.period_utils import compute_period
 
 
-def get_periods_by_observation_number(data: pd.DataFrame) -> pd.DataFrame:
+def compute_period_for_observations(data: pd.DataFrame, observations: int) -> tuple:
+    sample_data = data.sample(observations)
+    return observations, compute_period(sample_data, 'DATE-OBS', 'MAG_AUTO_NORM', 'MAGERR_AUTO')[0]
+
+
+def get_periods_by_observation_number(data: pd.DataFrame) -> dict:
     """
     Calculate the periods by number of observations for a given star,
     using a sample of the data with increasing size on each step.
     :param data: pd.DataFrame containing the data for the given star.
-    :return: Dict containing the periods by observation number.
+    :return: Dict containing the periods by observation number where 
+        the key is the number of observations and the value is the 
+        period obtained.
     """
     periods_by_observation_number = {}
-    observations = 5
-    while observations < len(data) + 5:
-        if observations > len(data):
-            observations = len(data)
-        periods_by_observation_number[observations], _ = compute_period(
-            data.sample(observations), 'DATE-OBS', 'MAG_AUTO_NORM', 'MAGERR_AUTO')
-        observations += 5
-    return periods_by_observation_number
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Prepare observation sizes
+        observation_sizes = list(range(5, len(data) + 5, 5))
+        if observation_sizes[-1] <= len(data):
+            observation_sizes.append(len(data))
+        else:
+            observation_sizes[-1] = len(data)
+
+        # Map each observation size to a future task
+        futures = {executor.submit(
+            compute_period_for_observations, data, observations): observations for observations in observation_sizes}
+
+        # Collect results as they complete
+        for future in concurrent.futures.as_completed(futures):
+            obs, period = future.result()
+            periods_by_observation_number[obs] = period
+
+    # Return sorted dict by observation number, since it may not be in order because of the multithreading
+    return dict(sorted(periods_by_observation_number.items()))
 
 
 def get_gaia_period(star_id: str) -> float:
